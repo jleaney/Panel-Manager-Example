3b2c1f4b2b1cec589dbf33a1949dd20ee607eac7
Jake <101182527@student.swin.edu.au>
2017-11-26 19:35:59 -0500

    Updated comments in panel manager script

--- Assets/PanelManager.cs
+++ Assets/PanelManager.cs
@@ -5,17 +5,14 @@ using UnityEngine.EventSystems;
 
 public class PanelManager : MonoBehaviour {
 
-    // this is an array of the panels that you want the buttons to open/close
-    public GameObject [] panels;
+    public GameObject [] panels; // this is an array of the panels that you want the buttons to open/close
 
-    // will hold the name of the tag on the pressed button
-    string selectedMenu;
+    string selectedMenu; // will hold the name of the tag on the pressed button
 
     // call this public method from the 'onClick()' function in the button component
     public void switchPanel () {
 
-        // gets the tag (a string) from the button that was pressed
-        selectedMenu = EventSystem.current.currentSelectedGameObject.tag;
+        selectedMenu = EventSystem.current.currentSelectedGameObject.tag;  // gets the tag (a string) from the button that was pressed
 
         // this loop will check every panel in the 'panel' array to see if it's name is the same 
         // as the tag on the button that was pressed
@@ -24,14 +21,12 @@ public class PanelManager : MonoBehaviour {
             // checks to see if the name of each panel is the same as the tag on the button that was just pressed
             if (panels[i].gameObject.name == selectedMenu)
             {
-                // turns panel ON if it is OFF, and vice versa
-                panels[i].SetActive(!panels[i].activeSelf);
+                panels[i].SetActive(!panels[i].activeSelf); // turns panel ON if it is OFF, and vice versa
             }
 
             else
             {
-                // turns the panel off
-                panels[i].SetActive(false);
+                panels[i].SetActive(false); // turns the correct panels off
             }
 
         }
